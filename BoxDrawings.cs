751186cb1a212675d7c8938a9942fc633f54b7c6
Admin <Admin@SRV01>
2022-07-04 21:25:59 -0300

    Adicionar arquivos de projeto.

---
+++ BoxDrawings.cs
@@ -0,0 +1,17 @@
+﻿namespace GL01
+{
+    internal class BoxDrawings
+    {
+        public static string LightHorizontal(int px)
+        {
+            string lh = "─";
+            if (px > 0)
+                for (int i = 0; i < px; i++)
+                {
+                    lh += lh;
+
+                }
+            return lh;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace GL01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(BoxDrawings.LightHorizontal(100));
+            
+        }
+
+    }
+}
